b5aa10603bec2f8362df896d56170dfa48fee8a9
cvieirasp <cvieirasp@gmail.com>
2024-12-26 18:53:09 -0300

    feat: initial flask restfull project

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+# Recurso para /users
+class UserList(Resource):
+    def get(self):
+        return {
+            'users': ['user1', 'user2', 'user3']
+        }
+    
+    def post(self):
+        return {
+            'message': 'User created'
+        }
+
+# Recurso para /users/{cpf}
+class User(Resource):    
+    def get(self, cpf):
+        return {
+            'user': cpf
+        }
+    
+api.add_resource(UserList, '/users')
+api.add_resource(User, '/users/<string:cpf>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
