cf4d426fec35292afbe815b73c4dc96c1900816b
cvieirasp <cvieirasp@gmail.com>
2024-12-29 13:16:05 -0300

    fix: grabbing PORT variable for heroku

--- Makefile
+++ Makefile
@@ -9,3 +9,8 @@ test:
 compose:
 	@docker compose build
 	@docker compose up
+
+heroku:
+	@heroku container:login
+	@heroku container:push -a $(APP) web
+	@heroku container:release -a $(APP) web
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from application import create_app
+import os
 
 app = create_app('application.config.Config')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=os.getenv('PORT', 5000))
